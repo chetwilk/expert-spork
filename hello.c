fc7886d57facae984982e5fb1dde31a028646987
chetwilk <chetswebmail@gmail.com>
2023-01-05 12:42:14 -0500

    Initial commit.

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*
+ * A simple hello world program for git repo setup.
+ * Licenced under GPL 2.
+ * Version 0.1 alpha.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    return 0;
+}
